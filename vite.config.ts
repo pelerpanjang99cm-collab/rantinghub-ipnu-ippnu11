78d07ec8c7a8faf2fb1c658abd54dd856e0b762a
pelerpanjang99cm-collab <pelerpanjang99cm@gmail.com>
2026-09-17 03:47:07 +0700

    Add Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,1 +1,14 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
+export default defineConfig({
+  plugins: [react()],
+  base: './',
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    rollupOptions: {
+      maxParallelFileOps: 128,
+    },
+  },
+});
